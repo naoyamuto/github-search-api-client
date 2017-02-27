03e35d77f49997c023e00d53788f8347c4de04d2
naoya_muto <naoya_muto@r.recruit.co.jp>
2017-02-27 12:41:24 +0900

    Initialize repository

---
+++ GithubSearchApiClient/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GithubSearchApiClient
+//
+//  Created by 武藤　尚也 on 2017/02/27.
+//  Copyright © 2017年 Naoya Muto. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
